6dfe537db05228499681b2eba7e843c28bd9ae2e
tomprater <tom.prater@carbonbrief.org>
2020-02-04 16:17:36 +0000

    Responsive tweaks 5 breakpoints

--- js/script.js
+++ js/script.js
@@ -9,12 +9,16 @@ function changeIframeSrc (screenWidth) {
     if (screenWidth > 1440) {
 
         iframe.attr('src',url + "retina.html");   
+
+    } else if ((screenWidth > 1024) && (screenWidth <= 1440)) {
     
-    } else if ((screenWidth >= 768) && (screenWidth <= 1440)) {
+        iframe.attr('src',url + "desktop.html");
+    
+    } else if ((screenWidth > 768) && (screenWidth <= 1024)) {
     
         iframe.attr('src',url + "laptop.html");   
 
-    } else if ((screenWidth > 570) && (screenWidth < 768)) {
+    } else if ((screenWidth > 570) && (screenWidth <= 768)) {
     
         iframe.attr('src',url + "tablet.html");   
 
